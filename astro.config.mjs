76fc648734e9b74d33db94493ad50c76051415ef
thewulf-swiss <mail@seo-webagentur.ch>
2026-05-12 00:28:15 +0200

    Fix: Added official site URL to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  // HIER DIE WICHTIGE ERGÄNZUNG:
+  site: 'https://www.swisspeakmedia.ch',
+  
   devToolbar: {
     enabled: false
   },
